4b767a61852e8caf05dbfaa828e6c2f0ab64cdd0
Marti <118295021+itdeveloppement@users.noreply.github.com>
2024-05-29 11:33:46 +0200

    maj arborescence

---
+++ app/views/error/err403.tpl.php
@@ -0,0 +1,5 @@
+<h1>403 Accès refusé</h1>
+
+<p>
+    Vous n'avez pas les droits pour accéder à cette page
+</p>
---
+++ app/views/error/err404.tpl.php
@@ -0,0 +1,5 @@
+<h1>403 Accès refusé</h1>
+
+<p>
+    Nous n'avons pas trouvé la page 
+</p>
