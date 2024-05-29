b148ca946b3cd2c7b4f0325e86772ac31dff21d8
Marti <118295021+itdeveloppement@users.noreply.github.com>
2024-05-29 14:32:40 +0200

    mise en place dossier tests

---
+++ tests/Test_accepatation.php
@@ -0,0 +1,15 @@
+<?php 
+/*
+Notions de test:
+test unitaire : test des methodes et des fonction des classes
+// role :
+// param :
+// se qui est attendu :
+// se qui est optenu :
+test d'integration : verifie l'interaction entre les modules sans se soucier du resultat des données
+test fonctionnel : verification les etats de sortie (valeurs) d'une fonction
+test d'acceptation : simulation du comportement de l'utilisateur
+
+*/
+
+// ----------------- tests d'aceptation ---------------------
