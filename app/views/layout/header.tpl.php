fdecf87c4017b1c15c57e4bd64caf03d5009f0df
Marti <118295021+itdeveloppement@users.noreply.github.com>
2024-06-12 12:28:32 +0200

    link css mis a jour

--- app/views/layout/header.tpl.php
+++ app/views/layout/header.tpl.php
@@ -15,6 +15,7 @@ Paramètres :
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../public/css/style.css">
+    <link rel="stylesheet" href="http://mcastellano.mywebecom.ovh/back/tickets/tickets_beta/public/css/style.css">
+   
     <title>Ticketing</title>
 </head>
